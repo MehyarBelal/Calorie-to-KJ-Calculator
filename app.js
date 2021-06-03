96d007e2d1e99dcc0b5559b04a6215079e7a7104
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2021-06-03 23:02:06 +1000

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let h2 = document.querySelector("h2");
 let span = document.querySelector(".spany");
 function checkBtn(Selected) {
   if (Selected == "Calorie") {
-    h2.innerHTML = Math.floor(value.value / 4.2) + " Calories";
+    h2.innerHTML = Math.floor(value.value / 4.184) + " Calories";
   } else if (Selected == "KJ") {
-    h2.innerHTML = Math.floor(value.value * 4.2) + " KJ";
+    h2.innerHTML = Math.floor(value.value * 4.184) + " KJ";
   } else {
     value.value = "";
   }
